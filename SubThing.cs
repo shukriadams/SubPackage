41c2f2574e11d9f24a3555d6bef86004c86d85cf
Shukri Adams <shukri.adams@gmail.com>
2025-09-15 20:27:30 +0200

    Update SubThing.cs

--- SubThing.cs
+++ SubThing.cs
@@ -1,8 +1,20 @@
 namespace SubPackage {
+    public static class DateTimeSpanExtensions
+    {
+        public static string Ago(this DateTime beforeUtc)
+        {
+            return "some value";
+        }
+    }
+
     public class Sub {
         public string Id { get;set;}
-
-        // v2 change
-        public string Name { get;set;}
+        
+        public Sub()
+        {
+            DateTime d = new DateTime();
+            // confirm extension property exists
+            string test = d.Ago();
+        }        
     }
 }
